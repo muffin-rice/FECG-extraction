fixed masking issue

src/models/FECGMem.py
@@ -129,7 +129,8 @@ class FECGMem(pl.LightningModule):
         # return all the losses with hyperparameters defined earlier
         return self.loss_params['fp_bce'] * torch.sum(results['loss_fecg_mask_bce']) / self.batch_size + \
                self.loss_params['fecg'] * torch.sum(results['loss_fecg_mae']) / self.batch_size + \
-               self.loss_params['fp_bce'] * self.loss_params['fp_bce_class'] * torch.sum(results['loss_fecg_mask_masked_bce'])
+               self.loss_params['fp_bce'] * self.loss_params['fp_bce_class'] * \
+               torch.sum(results['loss_fecg_mask_masked_bce']) / self.batch_size
 
     def calculate_losses_into_dict(self, recon_fecg : torch.Tensor, gt_fecg : torch.Tensor, recon_binary_fetal_mask : torch.Tensor,
                                    gt_binary_fetal_mask : torch.Tensor) -> {str : torch.Tensor}:

src/transforms.py
@@ -46,19 +46,12 @@ def calc_peak_mask(sig : np.array, peak_window = PEAK_SCALE, peak_sigma = PEAK_S
 
     for peak in peaks:
         peak = int(peak)
-        if peak >= 500:
+        if peak >= WINDOW_LENGTH*NUM_WINDOWS:
             break
         left, right = peak-int(peak_window/2), peak+int(peak_window/2)+1
         peak_mask[left : right] = gauss_kernel(peak_window, peak_sigma)[:len(peak_mask[left : right])]
 
         binary_peak_mask[peak] = 1
-
-        # if peak < binary_peak_window:
-        #     binary_peak_mask[0:peak+binary_peak_window]= 1
-        # elif peak + binary_peak_window >= binary_peak_mask.shape[0]:
-        #     binary_peak_mask[peak-binary_peak_window:] = 1
-        # else:
-        #     binary_peak_mask[peak-binary_peak_window:peak+binary_peak_window] = 1
 
     return peak_mask, binary_peak_mask
 
@@ -137,6 +130,7 @@ def get_signal_masks(signal):
 
 def split_signal_into_segments(signal):
     '''reshape signal into n_segments x segment for individual scaling'''
+
     signal['mecg_sig'] = signal['mecg_sig'].reshape(NUM_WINDOWS, WINDOW_LENGTH)
     signal['fecg_sig'] = signal['fecg_sig'].reshape(NUM_WINDOWS, WINDOW_LENGTH)
     signal['binary_fetal_mask'] = signal['binary_fetal_mask'].reshape(NUM_WINDOWS, WINDOW_LENGTH)

src/utils2.py
@@ -1,13 +1,12 @@
 from copy import deepcopy as copy
 import numpy as np
 import torch
-# from ecgdetectors import Detectors
+from ecgdetectors import Detectors
 from math import pi, sqrt, exp
 from scipy.signal import savgol_filter, filtfilt, firwin, firwin2, resample
 from numpy.random import normal, uniform
 
-# DETECTOR = Detectors(125).two_average_detector
-
+DETECTOR = Detectors(125).pan_tompkins_detector
 
 def filt(signal: np.ndarray, numtaps=31, sampling_rate=125):
     '''removes the baseline + power line noise'''
@@ -131,8 +130,7 @@ def invert_stft_batch(stft_sig : torch.Tensor): #batch_size x 34 x 469
 
 def return_peaks(sig : np.array):
     # returns the indices of the R peaks of a 1D array (depends on the algorithm)
-    return [1,2,3,4,5]
-    # return DETECTOR(sig)
+    return DETECTOR(sig)
 
 def calc_peak_stats(orig_peaks : [int], pred_peaks : [int], window_size : int = 7):
     # returns the indices of the R peaks of a 1D array (depends on the algorithm)